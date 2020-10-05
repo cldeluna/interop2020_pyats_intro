deb910a34b8845f2d3976ad3b8705e478c875d36
Claudia de Luna <claudia@indigowire.net>
2020-10-05 06:18:07 -0700

    Cleaned up and added comments

--- first_genie.py
+++ first_genie.py
@@ -13,6 +13,7 @@ __copyright__ = "Copyright (c) 2018 Claudia"
 __license__ = "Python"
 
 from genie.testbed import load
+# from unicon import Connection
 import json
 
 
@@ -26,6 +27,7 @@ def main():
     """
 
     # Instantiate the Testbed
+    # testbed = load('devnet_sbx_testbed_secure.yml')
     testbed = load('devnet_sbx_testbed.yml')
     print(f"\n======= TESTBED INFO =======\n")
     print(f"\tTestbed Value (object): {testbed}")
@@ -38,13 +40,14 @@ def main():
     # CLI & SAVE: genie parse "show version" --testbed-file "devnet_sbx_testbed.yml" --devices "sbx-n9kv-ao" --output PRE
     # DIFF CLI:  genie diff PRE POST
     device = testbed.devices['sbx-n9kv-ao']
+    # device = Connection(hostname='sbx-n9kv-ao', log_stdout=False)
     # print(dir(device))
     device.connect()
     response = device.parse('show version')
     print(f"\nResponse from sbx-n9kv-ao is of type {type(response)} and length {len(response)}")
-    print(response)
+    print(f"\n== RAW Response: \n{response}")
     print()
-    print(json.dumps(response, indent=4))
+    print(f"== Formatted Response: \n{json.dumps(response, indent=4)}")
     print(response.keys())
 
     # csr1000v-1 IOS-XE
@@ -53,34 +56,6 @@ def main():
     device.connect()
     response = device.parse('show version')
     print(f"\nResponse from csr1000v-1 is of type {type(response)} and length {len(response)}")
-    print(response)
-    print()
-    print(json.dumps(response, indent=4))
-    print(response.keys())
-
-
-    # csr1000v-1 IOS-XE
-    # CLI: genie parse "show version" --testbed-file devnet_sbx_testbed.yml --devices "csr1000v-1"
-    device = testbed.devices['csr1000v-1']
-    device.connect()
-    response = device.parse('show ip interface brief')
-    print(f"\nResponse from csr1000v-1 is of type {type(response)} and length {len(response)}")
-    print(response)
-    print()
-    print(json.dumps(response, indent=4))
-    print(response.keys())
-
-
-    # Sandbox NXOS Device
-    # CLI: genie parse "show version" --testbed-file "devnet_sbx_testbed.yml" --devices "sbx-n9kv-ao"
-    # This CLI command outputs the results into a directory called "out1" which does not have to exist
-    # CLI & SAVE: genie parse "show version" --testbed-file "devnet_sbx_testbed.yml" --devices "sbx-n9kv-ao" --output PRE
-    # DIFF CLI:  genie diff PRE POST
-    device = testbed.devices['sbx-n9kv-ao']
-    # print(dir(device))
-    device.connect()
-    response = device.parse('show ip interface brief')
-    print(f"\nResponse from sbx-n9kv-ao is of type {type(response)} and length {len(response)}")
     print(f"\n== RAW Response: \n{response}")
     print()
     print(f"== Formatted Response: \n{json.dumps(response, indent=4)}")
